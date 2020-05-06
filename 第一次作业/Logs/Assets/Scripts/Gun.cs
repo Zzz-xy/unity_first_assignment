28ff15ada3c6348a840888149123f42a07f6c68a
Zzz-xy <1270775799@users.noreply.github.com>
2020-05-06 10:21:50 +0800

    。

---
+++ 第一次作业/Logs/Assets/Scripts/Gun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Rigidbody2D rocket;
+    public float speed = 20f;
+
+    private PlayerControl playerCtrl;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = transform.root.gameObject.GetComponent<Animator>();
+        playerCtrl = transform.root.GetComponent<PlayerControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("Shoot");
+
+            if (playerCtrl.faceRight)
+            {
+                Rigidbody2D bullet = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                bullet.velocity = new Vector2(speed, 0);
+            }
+            else
+            {
+                Rigidbody2D bullet = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0, 0, 180)));
+                bullet.velocity = new Vector2(-speed, 0);
+            }
+        }
+    }
+}
---
+++ 第一次作业/Logs/Assets/Scripts/SetParticalSortingLayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetParticalSortingLayer : MonoBehaviour
+{
+    public string sortingLayerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = sortingLayerName;
+    }
+}
