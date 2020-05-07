0740536229c8d2fba34c9f40e2097f89eae48671
Zzz-xy <1270775799@users.noreply.github.com>
2020-05-07 22:24:48 +0800

    更新

---
+++ 第一次作业/Packages/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public Vector3 offset;
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransform.position + offset;
+    }
+}
