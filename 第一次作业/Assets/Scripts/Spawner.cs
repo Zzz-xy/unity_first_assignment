1f25cd6ffc5d6c62cb80f795b6eb3408cd7fdc77
Zzz-xy <1270775799@users.noreply.github.com>
2020-05-12 17:52:32 +0800

    周二

---
+++ 第一次作业/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float spawnTime = 3f;
+    public float spawnDelay = 4f;
+    public GameObject enemy;
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemy, transform.position, transform.rotation);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
